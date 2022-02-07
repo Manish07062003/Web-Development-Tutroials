7ec156cd4fb9d781f11a1d7a2705e27e1d42f0a4
Manish <20mcme11@uohyd.ac.in>
2022-02-08 00:38:05 +0530

    Handling errors in Express Apps

--- Express-Middleware/index.js
+++ Express-Middleware/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const appError = require('./appError');
 
 // app.use(morgan('dev'))
 
@@ -18,7 +19,8 @@ const verifyPassword = (req, res, next) => {
     if (password === 'Manish') {
         next();
     }
-    res.send("SORRY YOU NEED A PASSWORD")
+    // res.send("SORRY YOU NEED A PASSWORD")
+    throw new appError(401, 'Password Required');
 };
 
 // app.use((req, res, next) => { //app.use don't care about the path it recieves each an every requests from the browser
@@ -34,6 +36,9 @@ app.get('/', (req, res) => {
     console.log(`REQUEST DATE: ${req.requestTime}`)
     res.send('HOME PAGE');
 })
+app.get('/error', (req, res) => {
+    ChannelMergerNo.fly
+})
 app.get('/dogs', (req, res) => {
     console.log(`REQUEST DATE: ${req.requestTime}`)
     res.send('WOOF WOOF');
@@ -41,11 +46,26 @@ app.get('/dogs', (req, res) => {
 app.get('/secrets', verifyPassword, (req, res) => {  // we can pass multiple callback function to app.get() so we have initialised a funtion to verify the password
     res.send("MY SECRET IS: sometimes i wear headphones in public so i dont need to talk to anyone");
 })
-
+app.get('/admin', (req, res) => {
+    throw new appError(403, 'You are not an admin');
+})
 
 app.use((req, res) => { // this runs when nothing is sent to the response
     res.status(404).send("NOT FOUND!!");
 })
+
+// app.use(function (err, req, res, next) { // this is considered as error handling middleware (only prints when an error is thrown by express)
+//     console.log("*********************************");
+//     console.log("************ERROR****************");
+//     console.log("*********************************");
+//     next(err);
+// })
+
+app.use(function (err, req, res, next) { // this is considered as error handling middleware (only prints when an error is thrown by express)
+    const { status = 500, message = 'Something went wrong' } = err;
+    res.status(status).send(message);
+})
+
 
 app.listen(3000, () => {
     console.log("Listening to the port 3000");
